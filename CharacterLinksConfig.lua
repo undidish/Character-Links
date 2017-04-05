a810a4df1b152d1ed58cbe61341d45fb791c734d
Gérôme Nicolini <gerome.nicolini@gmail.com>
2017-04-05 17:16:20 +0200

    TOC update for 7.2

--- CharacterLinksConfig.lua
+++
@@ -1,7 +0,0 @@
-function WelcomeHome:OnInitialize()
-  LibStub("AceConfig-3.0"):RegisterOptionsTable("WelcomeHome", options)
-  self.optionsFrame = LibStub("AceConfigDialog-3.0"):AddToBlizOptions("WelcomeHome", "WelcomeHome")
-  self:RegisterChatCommand("wh", "ChatCommand")
-  self:RegisterChatCommand("welcomehome", "ChatCommand")
-  WelcomeHome.message = "Welcome Home!"
-end
